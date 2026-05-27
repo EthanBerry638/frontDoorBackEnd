5701400351d200b2c423961f250477f782b5136e
Ness <sampaio_88@hotmail.com>
2026-05-27 13:46:43 +0100

    Update Reed Controller and csproj file

--- firstDoorBackEnd/Controllers/ReedController.cs
+++ firstDoorBackEnd/Controllers/ReedController.cs
@@ -1,6 +1,33 @@
-﻿namespace firstDoorBackEnd.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using firstDoorBackEnd.Services;
+using firstDoorBackEnd.Repositories;
+using firstDoorBackEnd.Models;
+
+namespace firstDoorBackEnd.Controllers
+
 {
-    public class ReedController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReedController : ControllerBase
     {
+        private readonly ReedService _reedService;
+
+        public ReedController(ReedService reedService)
+        {
+            _reedService = reedService;
+        }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetJobsAsync()
+
+        {
+
+            var jobs = await _reedService.GetJobsAsync();
+
+            return Ok(jobs);
+
+        }
+
     }
 }
