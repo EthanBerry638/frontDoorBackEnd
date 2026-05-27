f420d3979c71dcfcbce90d752dc2ac712ed04390
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 11:11:02 +0100

    WIP: Add initial setup for testing

---
+++ firstDoorBackEnd.Tests/CareerJetRepositoryTests.cs
@@ -0,0 +1,27 @@
+﻿using firstDoorBackEnd.Repositories;
+using Moq;
+using Moq.Protected;
+
+namespace firstDoorBackEnd.Tests
+{
+    public class CareerJetRepositoryTests
+    {
+        private CareerJetRepository _careerJetRepository;
+
+        [Test]
+        public async Task GetJobsAsync_ShouldReturnListOfJobs_WhenExternalAPIReturnsListOfJobs()
+        {
+            var mockFactory = new Mock<IHttpClientFactory>();
+
+            var mockHttpMessageHanlder = new Mock<HttpMessageHandler>();
+            mockHttpMessageHanlder.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage
+                {
+                    StatusCode = System.Net.HttpStatusCode.OK
+                });
+
+            var client = new HttpClient(mockHttpMessageHanlder.Object);
+        }
+    }
+}
