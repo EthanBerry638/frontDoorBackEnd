6c70c79846f86c2f54a3459823a5e4b6accb7412
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 10:48:59 +0100

    Add implementation for testing

--- firstDoorBackEnd/Repositories/CareerJetRepository.cs
+++ firstDoorBackEnd/Repositories/CareerJetRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace firstDoorBackEnd.Repositories
+﻿using firstDoorBackEnd.Models;
+
+namespace firstDoorBackEnd.Repositories
 {
     public class CareerJetRepository : ICareerJetRepository
     {
+        public Task<List<Job>>? GetJobs()
+        {
+            return null;
+        }
     }
 }
--- firstDoorBackEnd/Repositories/ICareerJetRepository.cs
+++ firstDoorBackEnd/Repositories/ICareerJetRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace firstDoorBackEnd.Repositories
+﻿using firstDoorBackEnd.Models;
+
+namespace firstDoorBackEnd.Repositories
 {
     public interface ICareerJetRepository
     {
+        Task<List<Job>>? GetJobs();
     }
 }
