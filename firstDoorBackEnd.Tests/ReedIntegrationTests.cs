5c16e50e3fea5938038750700cc04e7528684429
Ness <sampaio_88@hotmail.com>
2026-05-27 13:44:12 +0100

    Add ReedIntegrationTests file

---
+++ firstDoorBackEnd.Tests/ReedIntegrationTests.cs
@@ -0,0 +1,16 @@
+﻿using Moq;
+namespace firstDoorBackEnd.Tests;
+
+public class ReedIntegrationTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
