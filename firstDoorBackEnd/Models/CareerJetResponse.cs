0e258e3b099586550ec4d32180862b022442d684
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 14:52:22 +0100

    Update properties to be lower case to match json and add message

--- firstDoorBackEnd/Models/CareerJetResponse.cs
+++ firstDoorBackEnd/Models/CareerJetResponse.cs
@@ -1,5 +1,10 @@
 ﻿namespace firstDoorBackEnd.Models
 {
-    public record CareerJetResponse(string Type, int Hits, int Pages, List<Job> Jobs)
+    public record CareerJetResponse(
+        string type, 
+        int hits, 
+        string message,
+        int pages, 
+        List<Job> jobs)
     {}
 }
