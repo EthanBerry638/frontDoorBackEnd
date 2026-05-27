ba4a8fc58693de3a31c8c742d981def23ad55f8f
Ethan Berry <ethanberry670@gmail.com>
2026-05-27 20:38:19 +0100

    Feat: Add referer header for later use

--- firstDoorBackEnd/Program.cs
+++ firstDoorBackEnd/Program.cs
@@ -15,6 +15,13 @@ builder.Services.AddHttpClient<ICareerJetRepository, CareerJetRepository>(client
     string base64Credentials = Convert.ToBase64String(binaryData);
 
     client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
+
+    string referer = builder.Configuration["CareerJet:Referer"] ?? string.Empty;
+
+    if (referer != string.Empty)
+    {
+        client.DefaultRequestHeaders.Add("Referer", referer);
+    }
 });
 
 builder.Services.AddEndpointsApiExplorer();
