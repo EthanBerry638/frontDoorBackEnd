506cf390214967ca33e63fdbc197203cc674afce
Ethan Berry <ethanberry670@gmail.com>
2026-05-28 12:23:42 +0100

    WIP: Configured client to add auth but still not working

--- firstDoorBackEnd/Program.cs
+++ firstDoorBackEnd/Program.cs
@@ -34,7 +34,20 @@ builder.Services.AddHealthChecks()
         uri: new Uri("https://search.api.careerjet.net/v4/query"),
         name: "careerjet-api",
         failureStatus: HealthStatus.Degraded,
-        tags: ["external"]);
+        tags: ["external"],
+        configureClient: (_, client) =>
+        {
+            string apiKey = builder.Configuration["CareerJet:ApiKey"] ?? string.Empty;
+            string authenticationString = $"{apiKey}:";
+
+            byte[] binaryData = System.Text.Encoding.UTF8.GetBytes(authenticationString);
+            string base64Credentials = Convert.ToBase64String(binaryData);
+
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", base64Credentials);
+
+            string referer = builder.Configuration["CareerJet:Referer"] ?? string.Empty;
+            client.DefaultRequestHeaders.Add("Referer", referer);
+        });
 
 builder.Services.AddTransient<ExceptionHandlingMiddleware>();
 
