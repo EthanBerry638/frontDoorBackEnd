169507a04123d36fc634b81e22683e112b8a729a
Ethan Berry <ethanberry670@gmail.com>
2026-05-28 14:29:25 +0100

    WIP: Add test, not working as expected yet though

--- firstDoorBackEnd.Tests/CareerJetServiceTests.cs
+++ firstDoorBackEnd.Tests/CareerJetServiceTests.cs
@@ -1,19 +1,36 @@
 ﻿using firstDoorBackEnd.Repositories;
 using firstDoorBackEnd.Services;
 using Moq;
+using firstDoorBackEnd.Models;
 
 namespace firstDoorBackEnd.Tests
 {
     public class CareerJetServiceTests
     {
         private CareerJetService _careerJetService;
+        private Mock<HttpClient> _mockHttpClient;
         private Mock<CareerJetRepository> _mockCareerJetRepository;
 
         [SetUp]
         public void SetUp()
         {
-            _mockCareerJetRepository = new Mock<CareerJetRepository>();
+            _mockHttpClient = new Mock<HttpClient>();
+            _mockCareerJetRepository = new Mock<CareerJetRepository>(_mockHttpClient.Object);
             _careerJetService = new CareerJetService(_mockCareerJetRepository.Object);
+        }
+
+        [Test]
+        public async Task GetAllJobsAsync_ShouldReturnEmptyList_WhenRepoIsCalledAndReturnsEmptyList()
+        {
+            var expectedList = new List<Job>();
+
+            _mockCareerJetRepository.Setup(repo => repo.GetAllJobsAsync("test", "test")).ReturnsAsync(expectedList);
+
+            var result = await _careerJetService.GetAllJobsAsync("test", "test");
+
+            Assert.That(result, Is.Empty);
+
+            _mockCareerJetRepository.Verify(repo => repo.GetAllJobsAsync("test", "test"), Times.Once());
         }
     }
 }
