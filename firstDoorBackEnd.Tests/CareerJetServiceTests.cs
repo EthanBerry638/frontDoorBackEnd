78bea814884a0a27c46df5b7c8977c46af1a8d7a
Ethan Berry <ethanberry670@gmail.com>
2026-05-28 13:58:58 +0100

    Feat: Add basic setup for service testing

---
+++ firstDoorBackEnd.Tests/CareerJetServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using firstDoorBackEnd.Repositories;
+using firstDoorBackEnd.Services;
+using Moq;
+
+namespace firstDoorBackEnd.Tests
+{
+    public class CareerJetServiceTests
+    {
+        private CareerJetService _careerJetService;
+        private Mock<CareerJetRepository> _mockCareerJetRepository;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCareerJetRepository = new Mock<CareerJetRepository>();
+            _careerJetService = new CareerJetService(_mockCareerJetRepository.Object);
+        }
+    }
+}
