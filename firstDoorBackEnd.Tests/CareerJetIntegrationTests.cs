e371a7ca3b10f039eff09b631f3ad4bba5df50c1
Ethan Berry <ethanberry670@gmail.com>
2026-05-29 09:59:00 +0100

    Feat: Add basic setup for integration testing

---
+++ firstDoorBackEnd.Tests/CareerJetIntegrationTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace firstDoorBackEnd.Tests
+{
+    public class CareerJetIntegrationTests
+    {
+        private WebApplicationFactory<Program> _factory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _factory.Dispose();
+        }
+    }
+}
