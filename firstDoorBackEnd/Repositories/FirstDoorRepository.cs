5ed5a9187983df877e1e1e39cbdb9f84a112e821
Ethan Berry <ethanberry670@gmail.com>
2026-06-01 10:16:34 +0100

    Setup basic files

---
+++ firstDoorBackEnd/Repositories/FirstDoorRepository.cs
@@ -0,0 +1,14 @@
+﻿using firstDoorBackEnd.Database;
+
+namespace firstDoorBackEnd.Repositories
+{
+    public class FirstDoorRepository : IFirstDoorRepository
+    {
+        private readonly FirstDoorContext _context;
+
+        public FirstDoorRepository(FirstDoorContext context)
+        {
+            _context = context; 
+        }
+    }
+}
---
+++ firstDoorBackEnd/Services/FirstDoorService.cs
@@ -0,0 +1,14 @@
+﻿using firstDoorBackEnd.Repositories;
+
+namespace firstDoorBackEnd.Services
+{
+    public class FirstDoorService : IFirstDoorService
+    {
+        private readonly IFirstDoorRepository _firstDoorRepository;
+
+        public FirstDoorService(IFirstDoorRepository firstDoorRepository)
+        {
+            _firstDoorRepository = firstDoorRepository;
+        }
+    }
+}
