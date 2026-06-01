57b51380f01c5503c910062e875f64bd1fa6e531
Ethan Berry <ethanberry670@gmail.com>
2026-06-01 11:47:56 +0100

    Feat: Add integration test

--- firstDoorBackEnd.Tests/FirstDoorIntegrationTests.cs
+++ firstDoorBackEnd.Tests/FirstDoorIntegrationTests.cs
@@ -3,6 +3,8 @@ using firstDoorBackEnd.Repositories;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using System.Text.Json;
 
@@ -31,6 +33,45 @@ namespace firstDoorBackEnd.Tests
         {
             var client = _factory.CreateClient();
             var expectedJobs = new List<SavedJob>();
+
+            var response = await client.GetAsync("api/FirstDoor");
+
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var jobs = JsonSerializer.Deserialize<List<SavedJob>>(content, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            jobs.Should().BeEquivalentTo(expectedJobs, options => options.Excluding(j => j.TimeSaved));
+
+            foreach (var job in jobs)
+            {
+                job.TimeSaved.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+            }
+        }
+
+        [Test]
+        public async Task GetAllSavedJobsAsyncEndpoint_ShouldReturnOkAndListOfSavedJobs_WhenDbIsSeeded()
+        {
+            var expectedJobs = new List<SavedJob>
+            {
+                new SavedJob { Id = 1, Title = "test", Description = "test", EmployerName = "test", Location = "test", Url = "test"},
+                new SavedJob { Id = 2, Title = "test", Description = "test", EmployerName = "test", Location = "test", Url = "test" },
+                new SavedJob { Id = 3, Title = "test", Description = "test", EmployerName = "test", Location = "test", Url = "test"}
+            };
+
+            var client = _factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureTestServices(services =>
+                {
+                    _mockRepository.Setup(repo => repo.GetAllSavedJobsAsync()).ReturnsAsync(expectedJobs);
+
+                    services.AddScoped(_ => _mockRepository.Object);
+                });
+            }).CreateClient();
 
             var response = await client.GetAsync("api/FirstDoor");
 
