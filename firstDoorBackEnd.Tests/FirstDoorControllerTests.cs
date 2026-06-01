626f37bbdb4e9c6690d472835f841dbccfbccaf6
Ethan Berry <ethanberry670@gmail.com>
2026-06-01 11:26:39 +0100

    Feat: Add new failing test

--- firstDoorBackEnd.Tests/FirstDoorControllerTests.cs
+++ firstDoorBackEnd.Tests/FirstDoorControllerTests.cs
@@ -1,6 +1,9 @@
 ﻿
 using firstDoorBackEnd.Controllers;
+using firstDoorBackEnd.Models;
 using firstDoorBackEnd.Services;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
 using Moq;
 
 namespace firstDoorBackEnd.Tests
@@ -15,6 +18,23 @@ namespace firstDoorBackEnd.Tests
         {
             _serviceMock = new Mock<IFirstDoorService>();
             _controller = new FirstDoorController(_serviceMock.Object);
+        }
+
+        [Test]
+        public async Task GetAllSavedJobsAsync_ShouldReturnOkWithEmptyList_WhenServiceReturnsEmptyList()
+        {
+            var expectedJobs = new List<SavedJob>();
+
+            _serviceMock.Setup(serv => serv.GetAllSavedJobsAsync()).ReturnsAsync(expectedJobs);
+
+            var result = await _controller.GetAllSavedJobsAsync();
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var value = okResult.Value as List<SavedJob>;
+
+            value.Should().BeEquivalentTo(expectedJobs);
+
+            _serviceMock.Verify(serv => serv.GetAllSavedJobsAsync(), Times.Once());
         }
     }
 }
