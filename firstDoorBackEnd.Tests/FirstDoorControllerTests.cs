6653eb785033406ab203314ba00696ddeac2f899
Ethan Berry <ethanberry670@gmail.com>
2026-06-01 10:28:01 +0100

    Add controller test file

---
+++ firstDoorBackEnd.Tests/FirstDoorControllerTests.cs
@@ -0,0 +1,20 @@
+﻿
+using firstDoorBackEnd.Controllers;
+using firstDoorBackEnd.Services;
+using Moq;
+
+namespace firstDoorBackEnd.Tests
+{
+    public class FirstDoorControllerTests
+    {
+        private Mock<IFirstDoorService> _serviceMock;
+        private FirstDoorController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _serviceMock = new Mock<IFirstDoorService>();
+            _controller = new FirstDoorController(_serviceMock.Object);
+        }
+    }
+}
